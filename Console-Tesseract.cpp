f1f17de46b2e42fd8030f606cc10f74f9e7c051c
warrior-coder <gudkou2003@gmail.com>
2021-07-11 09:51:07 +0300

    Set square font size

--- Console-Tesseract.cpp
+++ Console-Tesseract.cpp
@@ -2,8 +2,8 @@
 #include <windows.h>
 #include <cmath>
 
-#define C_VERT '#'
-#define C_EDGE '.'
+#define C_VERT static_cast<char>(219)
+#define C_EDGE '#'
 #define C_CLEAR ' '
 
 #define F_NUM_VERT 16
@@ -31,15 +31,15 @@ typedef struct
 class FIGURE
 {
 public:
-	VECT4 vertexes4[F_NUM_VERT] = {
-		{ 0, 0, 0, 0 },{ 1, 0, 0, 0 },{ 1, 1, 0, 0 },{ 0, 1, 0, 0 },{ 0, 0, 1, 0 },{ 1, 0, 1, 0 },{ 1, 1, 1, 0 },{ 0, 1, 1, 0 },
-		{ 0, 0, 0, 1 },{ 1, 0, 0, 1 },{ 1, 1, 0, 1 },{ 0, 1, 0, 1 },{ 0, 0, 1, 1 },{ 1, 0, 1, 1 },{ 1, 1, 1, 1 },{ 0, 1, 1, 1 }
-	};
 	VECT2 vertexes2[F_NUM_VERT] = {};
-	char edges[F_NUM_EDGE][2] = {
-		{ 0, 1 },{ 1, 2 },{ 2, 3 },{ 3, 0 },{ 4, 5 },{ 5, 6 },{ 6, 7 },{ 7, 4 },{ 0, 4 },{ 1, 5 },{ 2, 6 },{ 3, 7 },
-		{ 8, 9 },{ 9, 10 },{ 10, 11 },{ 11, 8 },{ 12, 13 },{ 13, 14 },{ 14, 15 },{ 15, 12 },{ 8, 12 },{ 9, 13 },{ 10, 14 },{ 11, 15 },
-		{ 0, 8 },{ 1, 9 },{ 2, 10 },{ 3, 11 },{ 4, 12 },{ 5, 13 },{ 6, 14 },{ 7, 15 }
+	VECT4 vertexes4[F_NUM_VERT] = {
+		{ 0, 0, 0, 0 }, { 1, 0, 0, 0 }, { 1, 1, 0, 0 }, { 0, 1, 0, 0 }, { 0, 0, 1, 0 }, { 1, 0, 1, 0 }, { 1, 1, 1, 0 }, { 0, 1, 1, 0 },
+		{ 0, 0, 0, 1 }, { 1, 0, 0, 1 }, { 1, 1, 0, 1 }, { 0, 1, 0, 1 }, { 0, 0, 1, 1 }, { 1, 0, 1, 1 }, { 1, 1, 1, 1 }, { 0, 1, 1, 1 }
+	};
+	unsigned char edges[F_NUM_EDGE][2] = {
+		{ 0, 1 },  { 1, 2 },  { 2, 3 },   { 3, 0 },  { 4, 5 },   { 5, 6 },   { 6, 7 },   { 7, 4 },   { 0, 4 },  { 1, 5 },  { 2, 6 },   { 3, 7 },
+		{ 8, 9 },  { 9, 10 }, { 10, 11 }, { 11, 8 }, { 12, 13 }, { 13, 14 }, { 14, 15 }, { 15, 12 }, { 8, 12 }, { 9, 13 }, { 10, 14 }, { 11, 15 },
+		{ 0, 8 },  { 1, 9 },  { 2, 10 },  { 3, 11 }, { 4, 12 },  { 5, 13 },  { 6, 14 },  { 7, 15 }
 	};
 
 	FIGURE()
@@ -54,16 +54,6 @@ public:
 	}
 	void rotate(float angle)
 	{
-		/*
-		Rotation matrix around XY example:
-
-		| cos, -sin,  0,    0 |      | X |     | cos * X - sin * Y |
-		| sin,  cos,  0,    0 |  *   | Y |  =  | sin * X + cos * Y |
-		| 0,    0,    1,    0 |      | Z |     |       1 * Z       |
-		| 0,    0,    0,    1 |      | W |     |       1 * W       |
-
-		*/
-
 		VECT4 vect4;
 
 		for (int i = 0; i < F_NUM_VERT; i++)
@@ -75,11 +65,8 @@ public:
 
 			vect4 = vertexes4[i];
 			
-			//Around XZ plane :
 			vertexes4[i].x = cos(angle) * vect4.x - sin(angle) * vect4.z;
 			vertexes4[i].z = sin(angle) * vect4.x + cos(angle) * vect4.z;	
-			
-			//Around YW plane :
 			vertexes4[i].y = cos(angle) * vect4.y - sin(angle) * vect4.w;
 			vertexes4[i].w = sin(angle) * vect4.y + cos(angle) * vect4.w;
 		
@@ -94,7 +81,8 @@ public:
 class SCREEN
 {
 private:
-	char char_pixel, buffer[S_WIDTH * S_HEIGHT + 1]; // One extra char for '\0'
+	char char_pixel, buffer[S_WIDTH * S_HEIGHT + 1];
+	HANDLE hout = nullptr;
 
 	void set_pixel(int x, int y)
 	{
@@ -111,7 +99,10 @@ private:
 
 		for (int i = 0; i < d; i++, x1 += sx, y1 += sy)
 		{
-			set_pixel(static_cast<int>(round(x1)), static_cast<int>(round(y1)));
+			set_pixel(
+				static_cast<int>(round(x1)),
+				static_cast<int>(round(y1))
+			);
 		}
 	}
 	VECT2 vect4_to_vect2(VECT4& vect4)
@@ -120,7 +111,6 @@ private:
 		VECT2 vect2;
 		float k;
 
-		// 4D -> 3D
 		if (vect4.w > 0)
 		{
 			k = S_CAMERA_W / (S_CAMERA_W + vect4.w);
@@ -135,8 +125,6 @@ private:
 			vect3.y = 50.0f + (vect4.y - 50.0f) / k;
 			vect3.z = 50.0f + (vect4.z - 50.0f) / k;
 		}
-
-		// 3D -> 2D
 		if (vect3.z > 0)
 		{
 			k = S_CAMERA_Z / (S_CAMERA_Z + vect3.z);
@@ -155,11 +143,18 @@ private:
 public:
 	SCREEN()
 	{
+		CONSOLE_FONT_INFOEX cfi = {};
+		cfi.dwFontSize = { 5, 4 };
+		cfi.cbSize = sizeof(CONSOLE_FONT_INFOEX);
+		hout = GetStdHandle(STD_OUTPUT_HANDLE);
+		SetCurrentConsoleFontEx(hout, false, &cfi);
+
 		system("mode con cols=100 lines=100");
-		system("title Console Tesseract");
+		system("title Tesseract");
 		system("color 0f");
 
 		buffer[S_WIDTH * S_HEIGHT] = '\0';
+		char_pixel = C_CLEAR;
 	}
 	void set_figure(FIGURE& figure)
 	{
@@ -185,7 +180,10 @@ public:
 		char_pixel = C_VERT;
 		for (int i = 0; i < F_NUM_VERT; i++)
 		{
-			set_pixel(static_cast<int>(round(figure.vertexes2[i].x)), static_cast<int>(round(figure.vertexes2[i].y)));
+			set_pixel(
+				static_cast<int>(round(figure.vertexes2[i].x)),
+				static_cast<int>(round(figure.vertexes2[i].y))
+			);
 		}
 	}
 	void clear()
@@ -194,7 +192,7 @@ public:
 	}
 	void print()
 	{
-		SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), { 0, 0 }); // Set the cursor at { 0, 0 } to write new frame on previous
+		SetConsoleCursorPosition(hout, { 0, 0 });
 		printf("%s", buffer);
 	}
 };
@@ -204,21 +202,13 @@ int main()
 	SCREEN scr;
 	FIGURE fig;
 
-	scr.clear();
-	scr.set_figure(fig);
-	scr.print();
-
-	Sleep(3000);
-	 
-	//while (GetKeyState(VK_ESCAPE) >= 0)
-	for (int a = 0; a < 360; a++)
+	while (GetKeyState(VK_ESCAPE) >= 0)
 	{
-		fig.rotate(0.0174533f);
+		fig.rotate(0.015f);
 		scr.clear();
 		scr.set_figure(fig);
 		scr.print();
 	}
-	Sleep(3000);
 
 	return 0;
 }
--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 Simple hypercube animation in console on clear C++.
 The result of combining [4-D mechanics](https://github.com/warrior-coder/CUBE-4D-TESSERACT) and [Console graphics](https://github.com/warrior-coder/CONSOLE-GRAPHICS-3D) together.
 
-![result](result.gif)
+![result2](result2.gif)
