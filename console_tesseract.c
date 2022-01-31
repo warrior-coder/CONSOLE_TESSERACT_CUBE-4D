fb72c14dd04ac2c366d8abf914310e5758b6d567
warrior-coder <gudkou2003@gmail.com>
2022-01-31 22:44:38 +0300

    english remarks

--- console_tesseract.c
+++ console_tesseract.c
@@ -1,24 +1,24 @@
 ﻿#include <stdio.h>
 #include <windows.h>
 #include <math.h>
-#pragma warning(disable : 4996) // отключаем предупреждения о небезопасных функциях
+#pragma warning(disable : 4996) // disable warnings about unsafe functions
 
 
-// определяем размеры консоли
+// define console sizes
 #define CON_WIDTH 96
 #define CON_HEIGHT 64
 
-// определяем свойства фигуры
+// define figure properties
 #define F_VERTEX_COUNT 16U
 #define F_EDGE_COUNT 32U
 #define F_EDGE_LENGTH 22.0f
 
-// определяем расстояния до камеры
+// define camera distances
 #define CAMERA_Z 80.0f
 #define CAMERA_W 25.0f
 
 
-// структуры координат на плоскости, в пространстве и в гиперпространстве
+// float coordinate structures on the plane, in space and in hyperspace
 typedef struct _FLOAT_PAIR
 {
 	float x, y;
@@ -34,30 +34,33 @@ typedef struct _FLOAT_QUAD
 	float x, y, z, w;
 }FLOAT_QUAD;
 
-// структура, хранящая индексы начала и конца линии
+// structure stores the indexes of begin and end of line path
 typedef struct _LINE_PATH_INDEX
 {
 	int begin, end;
 }LINE_PATH_INDEX;
 
 
-// объявляем глобальные переменные
+// declare global variables
 FLOAT_PAIR vertexes2[F_VERTEX_COUNT];
 FLOAT_QUAD vertexes4[F_VERTEX_COUNT] = {
 	{ -1, -1, -1, -1 }, {  1, -1, -1, -1 }, {  1,  1, -1, -1 }, { -1,  1, -1, -1 }, { -1, -1,  1, -1 }, {  1, -1,  1, -1 }, {  1,  1,  1, -1 }, { -1,  1,  1, -1 },
 	{ -1, -1, -1,  1 }, {  1, -1, -1,  1 }, {  1,  1, -1,  1 }, { -1,  1, -1,  1 }, { -1, -1,  1,  1 }, {  1, -1,  1,  1 }, {  1,  1,  1,  1 }, { -1,  1,  1,  1 }
 };
+
 LINE_PATH_INDEX indexes[F_EDGE_COUNT] = {
 	{  0,  1 }, {  1,  2 }, {  2,  3 }, {  3,  0 }, {  4,  5 }, {  5,  6 }, {  6,  7 }, {  7,  4 }, {  0,  4 }, {  1,  5 }, {  2,  6 }, {  3,  7 },
 	{  8,  9 }, {  9, 10 }, { 10, 11 }, { 11,  8 }, { 12, 13 }, { 13, 14 }, { 14, 15 }, { 15, 12 }, {  8, 12 }, {  9, 13 }, { 10, 14 }, { 11, 15 },
 	{  0,  8 }, {  1,  9 }, {  2, 10 }, {  3, 11 }, {  4, 12 }, {  5, 13 }, {  6, 14 }, {  7, 15 }
 };
+
 char buffer[CON_WIDTH * CON_HEIGHT];
-HANDLE hout = NULL;
-COORD coord = { 0, 0 };
+
+HANDLE hout = NULL; // handle to the console output
+COORD coord = { 0, 0 }; // carriage posintion in console
 
 
-// прототипы функций
+// function prototypes
 void consoleInit();
 void figrueInit();
 void figureRotate(float, float);
@@ -68,34 +71,35 @@ void bufferClear();
 void bufferShow();
 
 
-// точка входа консольного приложения 
+// console application entry point
 int main()
 {
 	const float angleXZ = 0.8f / 180.0f * 3.14159f;
 	const float angleYW = 1.0f / 180.0f * 3.14159f;
 
+	// initialize the application
 	consoleInit();
 	figrueInit();
 
-	// основной цикл программы
+	// main program loop
 	while (TRUE)
 	{
-		bufferClear(); // очищаем буфер
-		figureRotate(angleXZ, angleYW); // вращаем фигуру
-		bufferDrawFigure(); // рисуем фигуру в буфер
-		bufferShow(); // выводим буфер в консоль
+		bufferClear();
+		figureRotate(angleXZ, angleYW);
+		bufferDrawFigure();
+		bufferShow();
 		
-		Sleep(4);
+		Sleep(4); // pause in millisecinds
 	}
 
 	return 0;
 }
 
 
-// реализация функций
+// functions realization
 void consoleInit()
 {
-	// устанавливаем шрифт консоли
+	// setting console font
 	CONSOLE_FONT_INFOEX cfi = {
 		sizeof(CONSOLE_FONT_INFOEX),
 		0,
@@ -108,7 +112,7 @@ void consoleInit()
 	hout = GetStdHandle(STD_OUTPUT_HANDLE);
 	SetCurrentConsoleFontEx(hout, FALSE, &cfi);
 
-	// устанавливаем режим консоли
+	// setting console mode
 	char command[128];
 
 	sprintf(command, "mode con cols=%d lines=%d & title Console Tesseract & color 09", CON_WIDTH, CON_HEIGHT);
@@ -117,7 +121,7 @@ void consoleInit()
 
 void figrueInit()
 {
-	// масштабируем фигуру
+	// scaling the figure
 	for (int i = 0; i < F_VERTEX_COUNT; i++)
 	{
 		vertexes4[i].x *= F_EDGE_LENGTH / 2.0f;
@@ -135,11 +139,11 @@ void figureRotate(float angleXZ, float angleYW)
 	{
 		v4 = vertexes4[i];
 
-		// вращение в плоскости XZ
+		// rotation in XZ plane
 		vertexes4[i].x = cos(angleXZ) * v4.x - sin(angleXZ) * v4.z;
 		vertexes4[i].z = sin(angleXZ) * v4.x + cos(angleXZ) * v4.z;
 
-		// вращение в плоскости YW
+		// rotation in YW plane
 		vertexes4[i].y = cos(angleYW) * v4.y - sin(angleYW) * v4.w;
 		vertexes4[i].w = sin(angleYW) * v4.y + cos(angleYW) * v4.w;
 	}
@@ -151,18 +155,18 @@ FLOAT_PAIR perspectiveProject(FLOAT_QUAD* v4)
 	FLOAT_PAIR v2;
 	float k;
 
-	// проецируем на пространство
+	// projecting onto space
 	k = CAMERA_W / (CAMERA_W + v4->w);
 	v3.x = v4->x * k;
 	v3.y = v4->y * k;
 	v3.z = v4->z * k;
 
-	// проецируем на плоскость
+	// projecting onto plane
 	k = CAMERA_Z / (CAMERA_Z + v3.z);
 	v2.x = v3.x * k;
 	v2.y = v3.y * k;
 
-	// смещаем начало координат в центр консоли
+	// shifting the coordinates origin to the console center
 	v2.x += CON_WIDTH / 2.0f;
 	v2.y += CON_HEIGHT / 2.0f;
 
@@ -171,14 +175,17 @@ FLOAT_PAIR perspectiveProject(FLOAT_QUAD* v4)
 
 void bufferDrawLine(float x1, float y1, float x2, float y2)
 {
-	float d = fabsf(x2 - x1) > fabsf(y2 - y1) ? fabsf(x2 - x1) : fabsf(y2 - y1);
-	float dx = (x2 - x1) / d;
-	float dy = (y2 - y1) / d;
+	float d = fabsf(x2 - x1) > fabsf(y2 - y1) ? fabsf(x2 - x1) : fabsf(y2 - y1); // count the biggest axis difference
+	float dx = (x2 - x1) / d; // count difference X
+	float dy = (y2 - y1) / d; // count difference Y
 	int x, y;
 
-	for (int i = 0; i < d; i++, x1 += dx, y1 += dy)
+	for (float i = 0.0f; i < d; i += 1.0f, x1 += dx, y1 += dy)
 	{
-		if ((x = (int)x1) >= 0 && (y = (int)y1) >= 0 && x < CON_WIDTH && y < CON_HEIGHT)
+		x = (int)x1;
+		y = (int)y1;
+
+		if (x >= 0 && y >= 0 && x < CON_WIDTH && y < CON_HEIGHT)
 		{
 			buffer[y * CON_WIDTH + x] = '@';
 		}
@@ -187,13 +194,13 @@ void bufferDrawLine(float x1, float y1, float x2, float y2)
 
 void bufferDrawFigure()
 {
-	// проецируем вершины гиперпространства на плоскость консоли
+	// projecting the hyperspace vertixes onto the console plane
 	for (int i = 0; i < F_VERTEX_COUNT; i++)
 	{
 		vertexes2[i] = perspectiveProject(vertexes4 + i);
 	}
 
-	// рисуем ребра гиперкуба
+	// draw the hypercube edges
 	for (int i = 0; i < F_EDGE_COUNT; i++)
 	{
 		bufferDrawLine(
